5b5368e8e3496768b498049a78ba2ed95d89aea9
RichieAHB <richard.beddington@guardian.co.uk>
2019-11-15 13:54:23 +0000

    Add missing toggle

--- server.tsx
+++ server.tsx
@@ -9,11 +9,22 @@ import { exampleArticle } from "./model/Article";
 
 const app = express();
 
+const passthrough = (arr: TemplateStringsArray, ...strs: string[]) => {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i] || ""}${strs[i] || ""}`;
+  }
+  return str;
+};
+
+const html = passthrough;
+const js = passthrough;
+
 app.get("/", (req, res) => {
   const { pillar, type } = req.query;
   const sheet = new ServerStyleSheet();
   try {
-    const html = ReactDOMServer.renderToStaticMarkup(
+    const body = ReactDOMServer.renderToStaticMarkup(
       sheet.collectStyles(
         <>
           <ThemeProvider theme={getTheme(pillar)}>
@@ -27,7 +38,39 @@ app.get("/", (req, res) => {
       )
     );
     const styleTags = sheet.getStyleTags();
-    res.send(`<html><head>${styleTags}</head><body>${html}</body></html>`);
+    res.send(html`
+      <html>
+        <head>
+          ${styleTags}
+        </head>
+        <body>
+          ${body}
+        </body>
+        <script>
+          ${js`
+          /**
+           * We need to add some custom JS here as, for now, we wont' be
+           * hydrating the React on the client
+           *
+           * So for any interactivity it will need to be added with vanilla JS
+           */
+          window.addEventListener("load", () => {
+            [...document.querySelectorAll("[data-toggle]")].forEach(toggler => {
+              toggler.addEventListener("click", () => {
+                const selector = toggler.dataset.toggle;
+                const els = document.querySelectorAll(selector);
+                [...els].forEach(el => {
+                  el.dataset.open = !(
+                    el.dataset.open && JSON.parse(el.dataset.open)
+                  );
+                });
+              });
+            });
+          });
+          `};
+        </script>
+      </html>
+    `);
   } catch (error) {
     // handle error
     console.error(error);
